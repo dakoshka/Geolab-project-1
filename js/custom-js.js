0ae9412833f383ec151284e9fe0a216fc408dc71
dakoshka <dako.sidamonidze@gmail.com>
2018-01-06 23:47:04 +0400

    added validation messages

--- js/custom-js.js
+++ js/custom-js.js
@@ -64,3 +64,32 @@ function submitClicked(){
 }
 
 
+// The latitude and longitude of your business / place
+var position = [41.717578, 44.7828302];
+
+function showGoogleMaps() {
+
+    var latLng = new google.maps.LatLng(position[0], position[1]);
+
+    var mapOptions = {
+        zoom: 16, // initialize zoom level - the max value is 21
+        streetViewControl: false, // hide the yellow Street View pegman
+        scaleControl: true, // allow users to zoom the Google Map
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        center: latLng
+    };
+
+    map = new google.maps.Map(document.getElementById('googlemaps'),
+        mapOptions);
+
+    // Show the default red marker at the location
+    marker = new google.maps.Marker({
+        position: latLng,
+        map: map,
+        draggable: false,
+        animation: google.maps.Animation.DROP
+    });
+}
+
+google.maps.event.addDomListener(window, 'load', showGoogleMaps);
+
